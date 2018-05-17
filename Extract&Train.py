2f1f17bb5e22c2a1540d14a40e55408f03538bf1
Ohad Doron <ogdoron@gmail.com>
2018-05-17 09:35:03 +0300

    image histogram
    90% accuracy using image histograms after bootstrapping images

--- Extract&Train.py
+++ Extract&Train.py
@@ -10,7 +10,9 @@ import shutil
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Activation,Dropout
+from sklearn.model_selection import train_test_split
+from progressbar import ProgressBar
 
 path = r'C:\Users\ohaddoron\Documents\CV Project\busesRect'
 annotations = path + '/bus_color.txt'
@@ -28,7 +30,7 @@ def load(path):
     X = []
     y = []
     for label in os.listdir(path):
-        files = [file for file in os.listdir(path + '/' + label) if file.endswith('.jpg')]
+        files = [file for file in os.listdir(path + '/' + label) if file.endswith('.jpeg')]
         for file in files:
             tmp = []
             img = cv2.imread(path + '/' + label + '/' + file)
@@ -36,21 +38,65 @@ def load(path):
             for i,col in enumerate(color):
                 histr = cv2.calcHist([img],[i],None,[256],[0,256])
                 tmp.append(histr)
+                #img = cv2.resize(img,(200,200))
+                #tmp.append(img.ravel())
             X.append(np.transpose(np.vstack(tmp)))
-            y.append(int(label))
-    return np.vstack(X),y    
+            
+            y.append(int(label)-1)
+    return np.vstack(X),y   
 
-X_train,y_train = load(path)
-
-model = Sequential([
-    Dense(200, input_shape=(X_train.shape[1],)),
-    Activation('tanh'),
-    Dense(len(np.unique(y_train))),
-    Activation('softmax'),
-])
+def genImages(path):
+    datagen = ImageDataGenerator(
+            rotation_range=40,
+            width_shift_range=0.2,
+            height_shift_range=0.2,
+            rescale=1./255,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True,
+            fill_mode='nearest')
     
-model.compile(optimizer='adam',
-          loss='categorical_crossentropy',
-          metrics=['accuracy'])
+    for label in os.listdir(path):
+        files = [file for file in os.listdir(path + '/' + label) if file.endswith('.jpg')]
+        for file in files:
+            img = load_img(path + '/' + label + '/' + file)  # this is a PIL image
+            x = img_to_array(img)  # this is a Numpy array with shape (3, 150, 150)
+            x = x.reshape((1,) + x.shape)  # this is a Numpy array with shape (1, 3, 150, 150)
+            
+            # the .flow() command below generates batches of randomly transformed images
+            # and saves the results to the `preview/` directory
+            i = 0
+            for batch in datagen.flow(x, batch_size=1,
+                                      save_to_dir=path + '/' + label, save_prefix=label, save_format='jpeg'):
+                i += 1
+                if i > 20:
+                    break  # otherwise the generator would loop indefinitely 
 
-model.fit(X_train, y_train, epochs=10, batch_size=32)
+pbar = ProgressBar()
+scores = []
+for i in pbar(range(10)):
+    X_train,y_train = load(path)
+    X_train,X_test,y_train,y_test = train_test_split(X_train,y_train,test_size=0.1)
+    
+    model = Sequential([
+        Dense(1000, input_shape=(X_train.shape[1],)),
+        Activation('tanh'),
+        Dropout(0.0),
+        Dense(len(np.unique(y_train))),
+        Activation('softmax'),
+    ])
+        
+    model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+    
+    model.fit(X_train, y_train, epochs=10, batch_size=100,shuffle=True,verbose=1)
+    out = model.predict_classes(X_test)
+    score = 0
+    for o,yi in zip(out,y_test):
+        if o == yi:
+            score+=1
+    scores.append(score/len(out)*100)
+    print('\n' + str(scores[-1]))
+        
+    
